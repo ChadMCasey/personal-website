7ddb7365ab45a7f09857e162e00e1615ff2868b9
Chad Casey <gbchadd@gmail.com>
2024-05-26 22:41:25 -0700

    merge

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,8 @@
 import "./App.css";
 
 function App() {
-<<<<<<< HEAD:src/App.jsx
-  return <div className="app"></div>;
-=======
   return;
   <div className="app"></div>;
->>>>>>> 1c688c1e6fd3b3d99c3d55fac94447dac7ee63fb:src/components/App/App.jsx
 }
 
 export default App;
